2a6f8ec523e46568154dbfdafde56861d8cbb447
loopyme <peter@mail.loopy.tech>
2020-02-18 11:23:55 +0800

    add unittest for func/cls

--- source/index.rst
+++ source/index.rst
@@ -1,7 +1,7 @@
 
 
-LoopyCryptor
-============
+LoopyCryptor-0.1.0
+==================
 
 .. toctree::
    :maxdepth: 3
@@ -10,7 +10,7 @@ LoopyCryptor
 Introduction
 ============
 
-What I've done here is wrap part of `pycryptodome` functions up with a 'pythonic' interface, which is easy to use. What's more, it can automatically determine the type of input(`bytes`or`str`), handle it properly and gives the output of the given type. 
+What I've done here is wrap part of `pycryptodome` and `pickle` functions up with a 'pythonic' interface, which is easy to use. What's more, it can encrypt **any kinds of python objects**, handle it properly and gives the output of the given type. 
 
 In short, I am trying to make a Cryptor that can help me as much as possible not to think about `bytes`, `string`, `byte boundary` and so many problems when I am trying to build a encrypted chat room.
 
@@ -27,33 +27,47 @@ All functions are implemented in Class:Cryptor, but we use lambda functions to w
 
     import loopyCryptor
 
-    ###############################################################################
-    # RSA
-    
     # generate a pair of key
     public_key, private_key = loopyCryptor.generate_RSA_key()
 
     # encrypt
-    text = "I hate verilog."
-    cipher_byte = loopyCryptor.RSA_encrypt(text,public_key)
+    obj = [123,"HELLO",None,{"How are you?":b"I am fine and you?"}]
+    cipher_byte = loopyCryptor.RSA_encrypt(obj,public_key)
 
     # decrypt
-    decrypt_text = loopyCryptor.RSA_decrypt(cipher_byte,private_key)
-    print(decrypt_text) # I hate verilog.
+    decrypt_obj = loopyCryptor.RSA_decrypt(cipher_byte,private_key)
+
+    # result
+    print(decrypt_obj)
+    print(decrypt_obj==obj)
+    print(list(map(type, decrypt_obj)))
+    
+    # [123, 'HELLO', None, {'How are you?': b'I am fine and you?'}] 
+    # True 
+    # [<class 'int'>, <class 'str'>, <class 'NoneType'>, <class 'dict'>]
 
     ###############################################################################
     # AES
     
+    import loopyCryptor
     # generate a key
     AES_key = loopyCryptor.generate_AES_key()
 
     # encrypt
-    text = "I hate verilog."
-    cipher_byte = loopyCryptor.AES_encrypt(text,AES_key)
+    obj = [123,"HELLO",None,{"How are you?":b"I am fine and you?"}]
+    cipher_byte = loopyCryptor.AES_encrypt(obj,AES_key)
 
     # decrypt
-    decrypt_text = loopyCryptor.AES_decrypt(cipher_byte,AES_key)
-    print(decrypt_text) # I hate verilog.
+    decrypt_obj = loopyCryptor.AES_decrypt(cipher_byte,AES_key)
+
+    # result
+    print(decrypt_obj)
+    print(decrypt_obj==obj)
+    print(list(map(type, decrypt_obj))) 
+
+    # [123, 'HELLO', None, {'How are you?': b'I am fine and you?'}] 
+    # True 
+    # [<class 'int'>, <class 'str'>, <class 'NoneType'>, <class 'dict'>]
 
 API
 ===
--- test/test.py
+++ test/test.py
@@ -17,11 +17,15 @@ class foo:
         return self.value1 == other.value1 and self.value2 == other.value2 and self.cls_value == other.cls_value
 
 
+def bar(n):
+    return n * bar(n - 1) if n != 1 else 1
+
+
 class MyTestCase(unittest.TestCase):
 
     @staticmethod
     def random_str():
-        return (''.join(choice(string.ascii_letters + string.digits) for _ in range(randint(10, 1000))))
+        return ''.join(choice(string.ascii_letters + string.digits) for _ in range(randint(10, 1000)))
 
     @staticmethod
     def random_list():
@@ -35,39 +39,45 @@ class MyTestCase(unittest.TestCase):
     def random_obj():
         return foo(randint(1, 100), MyTestCase.random_byte(), MyTestCase.random_str())
 
+    @staticmethod
+    def ret_cls():
+        return foo
+
+    @staticmethod
+    def ret_func():
+        return bar
+
     def test_AES(self):
         from loopyCryptor import AES_encrypt, AES_decrypt, set_default_AES_key, generate_AES_key
 
         for random_obj in [MyTestCase.random_byte, MyTestCase.random_obj, MyTestCase.random_str,
-                           MyTestCase.random_list]:
-            for i in range(3):
-                key = generate_AES_key()
-                text = random_obj()
-                encrypt_text = AES_encrypt(text, key)
-                self.assertEqual(text, AES_decrypt(encrypt_text, key))
+                           MyTestCase.random_list, MyTestCase.ret_cls, MyTestCase.ret_func]:
+            key = generate_AES_key()
+            text = random_obj()
+            encrypt_text = AES_encrypt(text, key)
+            self.assertEqual(text, AES_decrypt(encrypt_text, key))
 
-                key = generate_AES_key()
-                set_default_AES_key(key)
-                text = random_obj()
-                encrypt_text = AES_encrypt(text)
-                self.assertEqual(text, AES_decrypt(encrypt_text))
+            key = generate_AES_key()
+            set_default_AES_key(key)
+            text = random_obj()
+            encrypt_text = AES_encrypt(text)
+            self.assertEqual(text, AES_decrypt(encrypt_text))
 
     def test_RSA(self):
         from loopyCryptor import RSA_encrypt, RSA_decrypt, set_default_RSA_key, generate_RSA_key
 
         for random_obj in [MyTestCase.random_byte, MyTestCase.random_obj, MyTestCase.random_str,
-                           MyTestCase.random_list]:
-            for i in range(3):
-                key = generate_RSA_key()
-                text = random_obj()
-                encrypt_text = RSA_encrypt(text, key[0])
-                self.assertEqual(text, RSA_decrypt(encrypt_text, key[1]))
+                           MyTestCase.random_list, MyTestCase.ret_cls, MyTestCase.ret_func]:
+            key = generate_RSA_key()
+            text = random_obj()
+            encrypt_text = RSA_encrypt(text, key[0])
+            self.assertEqual(text, RSA_decrypt(encrypt_text, key[1]))
 
-                key = generate_RSA_key()
-                set_default_RSA_key(*key)
-                text = random_obj()
-                encrypt_text = RSA_encrypt(text)
-                self.assertEqual(text, RSA_decrypt(encrypt_text))
+            key = generate_RSA_key()
+            set_default_RSA_key(*key)
+            text = random_obj()
+            encrypt_text = RSA_encrypt(text)
+            self.assertEqual(text, RSA_decrypt(encrypt_text))
 
 
 if __name__ == '__main__':
